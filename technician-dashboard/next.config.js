5815e59010edc47e13a040edfefa043b8e127c2f
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-01-12 09:37:45 +0700

    Update next config

--- technician-dashboard/next.config.js
+++ technician-dashboard/next.config.js
@@ -2,13 +2,18 @@
 const nextConfig = {
   swcMinify: true,
   images: {
+    formats: ['image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
       },
     ],
+    deviceSizes: [425, 576, 769, 992, 1200, 1920],
+    imageSizes: [190, 240, 330, 450],
+    minimumCacheTTL: 90,
   },
+  productionBrowserSourceMaps: true,
 }
 
 module.exports = nextConfig
